c43b1b9850c1b51ad51e1c6ef18ad9d670af07f6
polvdw <91751232+polvdw@users.noreply.github.com>
2024-02-16 12:41:55 +0100

    Test display image

--- function.js
+++ function.js
@@ -1,2 +1,13 @@
-console.log('avatar');
-console.log(88);
+function displayImage() {
+    var input = document.getElementById('imageInput');
+    var image = document.getElementById('displayedImage');
+
+    var file = input.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+        image.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+}
